f8ca31240df9c0233dbbe9c50ce7f877758b6706
Jakobf-1 <Jakobf@schwienbacher.org>
2026-07-22 16:30:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ git clone https://github.com/Jakobf-1/JsOS-Ubuntu- ~/tmp_jsos
 cp -r ~/tmp_jsos/* ~/.config/
 sudo systemctl disable gdm3
 cat > ~/.bash_profile <<'EOF'
-# Load .bashrc
 if [ -f ~/.bashrc ]; then
     . ~/.bashrc
 fi
-
-# Auto-start Hyprland on tty1
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     exec Hyprland
 fi
 EOF
+echo "Finished installing JsOS! Restarting in 3 seconds!"
+sleep 3
+reboot
